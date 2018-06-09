f6f9e171c2be8999a100c61fb20f8650d6ac1568
Priscilla H <priscillamhuynh@gmail.com>
2018-06-09 06:02:25 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,7 @@ def turn_count(board)
   count = 0
   board.each do |occupied_positions|
     count +=1
-    puts "#{board.length}.t_i"
+    puts "#{occupied_positions.length}"
     # turns +=  occupied_positions <= board.length-1
     # puts "#{turn_count(occupied_positions)}"
   end
